f853934432cfc79667ccee50f5aba82f812e5d92
Florian Ragwitz <rafl@debian.org>
2008-12-07 00:16:41 +0000

    Import of FLORA/Class-C3-Adopt-NEXT-0.04 from CPAN.
    gitpan-cpan-distribution: Class-C3-Adopt-NEXT
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         FLORA/Class-C3-Adopt-NEXT-0.04.tar.gz
    gitpan-cpan-author:       FLORA
    gitpan-cpan-maturity:     released

--- lib/Class/C3/Adopt/NEXT.pm
+++ lib/Class/C3/Adopt/NEXT.pm
@@ -7,7 +7,7 @@ use NEXT;
 use MRO::Compat;
 use warnings::register;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 {
     my %c3_mro_ok;
@@ -51,6 +51,8 @@ our $VERSION = '0.03';
             goto &next::method if $wanted_class =~ /^NEXT:.*:ACTUAL/;
             goto &maybe::next::method;
         };
+
+        *NEXT::ACTUAL::AUTOLOAD = \&NEXT::AUTOLOAD;
     }
 
     sub unimport {
